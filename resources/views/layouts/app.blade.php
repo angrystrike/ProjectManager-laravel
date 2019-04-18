2f03314fdee616d31f4d8ea5902888ba6c2ee2ee
angrystrike <melnik.max00@gmail.com>
2019-04-18 21:35:27 +0300

    layout changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" >
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -11,7 +11,7 @@
 
     <!-- Scripts -->
 
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,83 +19,87 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ URL::asset('css/common.css') }}" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ URL::asset('css/common.css') }}"/>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Manager') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<div id="app">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Manager') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                    </ul>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+                </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
+                <ul class="navbar-nav">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}"><i class="fas fa-sign-in-alt"></i> {{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}"><i class="fas fa-sign-in-alt"></i> {{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('register') }}"><i class="fas fa-user-plus"></i> {{ __('Register') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}"><i class="fas fa-user-plus"></i> {{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item"> <a class="nav-link" href="{{ route('companies.index') }}"><i class="fas fa-building"></i> My Companies</a></li>
-                            <li class="nav-item"> <a class="nav-link"  href="{{ route('projects.index') }}"><i class="fas fa-briefcase"></i> Projects</a></li>
-                            <li class="nav-item"> <a class="nav-link" href="{{ route('tasks.index') }}"><i class="fas fa-tasks"></i> Tasks</a></li>
+                        @endif
+                    @else
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('search') }}"><i class="fas fa-search"></i> Search</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="/users/{{ Auth::user()->id }}"><i class="fas fa-building"></i> My Profile</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('companies.index') }}"><i class="fas fa-building"></i> My Companies</a></li>
+                        <li class="nav-item"> <a class="nav-link"  href="{{ route('projects.index') }}"><i class="fas fa-briefcase"></i> My Projects</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('tasks.index') }}"><i class="fas fa-tasks"></i> My Tasks</a></li>
 
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre><i class="fas fa-id-badge"></i>
-                                     {{ Auth::user()->name }} <span class="caret"></span>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre><i class="fas fa-id-badge"></i>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                @if (Auth::user()->role_id == 1)
+                                    <a class="dropdown-item" href="{{ route('admin.companies') }}"><i class="fas fa-building"></i> All Companies</a>
+                                    <a class="dropdown-item" href="{{ route('admin.projects') }}"><i class="fas fa-briefcase"></i> All Projects</a>
+                                    <a class="dropdown-item" href="{{ route('admin.tasks') }}"><i class="fas fa-tasks"></i> All Tasks</a>
+                                    <a class="dropdown-item" href="{{ route('admin.comments') }}"><i class="fas fa-comments"></i> All Comments</a>
+                                    <a class="dropdown-item" href="{{ route('admin.users') }}"><i class="fas fa-user-tag"></i> All Users</a>
+                                @endif
+
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                    <i class="fas fa-sign-out-alt"></i> {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    @if (Auth::user()->role_id == 1)
-                                        <a class="dropdown-item" href="{{ route('companies.all') }}"><i class="fas fa-building"></i> All Companies</a>
-                                        <a class="dropdown-item" href="{{ route('projects.all') }}"><i class="fas fa-briefcase"></i> All Projects</a>
-                                        <a class="dropdown-item" href="{{ route('tasks.all') }}"><i class="fas fa-tasks"></i> All Tasks</a>
-                                        <a class="dropdown-item" href="{{ route('companies.index') }}"><i class="fas fa-comments"></i> All Comments</a>
-                                        <a class="dropdown-item" href="{{ route('companies.index') }}"><i class="fas fa-user-tag"></i> All Roles</a>
-                                    @endif
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
 
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        <i class="fas fa-sign-out-alt"></i> {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
             </div>
-        </nav>
+        </div>
+    </nav>
+</div>
 
-        <main class="py-4">
-            <div class="container">
-                @include('partials.errors')
-                @include('partials.success')
-                <div class="row">
-                    @yield('content')
-                </div>
-            </div>
-        </main>
+<main class="py-4">
+    <div class="container">
+        @include('partials.errors')
+        @include('partials.success')
+        <div class="row">
+            @yield('content')
+        </div>
     </div>
+</main>
+
 </body>
 </html>
