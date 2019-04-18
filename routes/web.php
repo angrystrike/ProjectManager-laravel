a08f877203bfc54c1390acdf9d6c3a7436aaf2fe
angrystrike <melnik.max00@gmail.com>
2019-04-18 21:45:32 +0300

    isAdmin middleware added

--- routes/web.php
+++ routes/web.php
@@ -4,27 +4,46 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/regex', 'RegexController@index');
 
-Route::get('companies/all', 'CompaniesController@all')->name('companies.all');
-Route::resource('companies', 'CompaniesController');
+Route::middleware(['auth'])->group(function () {
 
-Route::get('projects/all', 'ProjectsController@all')->name('projects.all');
-Route::get('projects/create/{company_id?}', 'ProjectsController@create');
-Route::post('projects/adduser', 'ProjectsController@addUser')->name('projects.adduser');
-Route::delete('projects/member/{project_id}/{user_id}', 'ProjectsController@deleteMember')->name('projects.deleteMember');
-Route::resource('projects', 'ProjectsController');
+    Route::get('search', 'SearchController@search')->name('search');
 
-Route::resource('roles', 'RolesController');
+    Route::middleware(['admin'])->group( function () {
+        Route::get('companies/all', 'CompaniesController@all')->name('admin.companies');
+    });
+    Route::resource('companies', 'CompaniesController');
 
-Route::get('tasks/all', 'TasksController@all')->name('tasks.all');
-Route::get('tasks/create/{project_id?}', 'TasksController@create');
-Route::post('tasks/adduser', 'TasksController@adduser')->name('tasks.adduser');
-Route::resource('tasks', 'TasksController');
-Route::resource('users', 'UsersController');
-Route::resource('comments', 'CommentsController');
+    Route::middleware(['admin'])->group( function () {
+        Route::get('projects/all', 'ProjectsController@all')->name('admin.projects');
+    });
+    Route::get('projects/create/{company_id?}', 'ProjectsController@create');
+    Route::post('projects/addUser', 'ProjectsController@addUser')->name('projects.addUser');
+    Route::delete('projects/member/{project_id}/{user_id}', 'ProjectsController@deleteMember')->name('projects.deleteMember');
+    Route::resource('projects', 'ProjectsController');
+
+    Route::middleware(['admin'])->group( function () {
+        Route::get('tasks/all', 'TasksController@all')->name('admin.tasks');
+    });
+
+    Route::get('tasks/create/{project_id?}', 'TasksController@create');
+    Route::post('tasks/addUser', 'TasksController@addUser')->name('tasks.addUser');
+    Route::delete('tasks/member/{task_id}/{user_id}', 'TasksController@deleteMember')->name('tasks.deleteMember');
+    Route::resource('tasks', 'TasksController');
+
+    Route::middleware(['admin'])->group( function () {
+        Route::get('users/all', 'UsersController@all')->name('admin.users');
+    });
+
+    Route::resource('users', 'UsersController');
+
+    Route::middleware(['admin'])->group( function () {
+        Route::get('comments/all', 'CommentsController@all')->name('admin.comments');
+    });
+    Route::resource('comments', 'CommentsController');
+});
 
