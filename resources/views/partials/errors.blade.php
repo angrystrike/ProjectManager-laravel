8f94d5ab759feea7389986cf7d3650ee31b942af
angrystrike <melnik.max00@gmail.com>
2019-04-19 17:07:15 +0300

    errors minor changes

--- resources/views/partials/errors.blade.php
+++ resources/views/partials/errors.blade.php
@@ -1,18 +1,3 @@
-{{--
-@if ( isset($errors) && $errors)
-    <div class="alert alert-dismissable alert-danger">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-        @dump($errors)
-       --}}
-{{-- @foreach ($errors->all() as $error)
-            <li><strong>{!! $error !!}</strong></li>
-        @endforeach--}}{{--
-
-    </div>
-@endif
---}}
 @if (session()->has('errors'))
     <div class="alert alert-dismissable alert-danger">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
